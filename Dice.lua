eed41c55a99712421bedfa9158ca241b78f9c906
helado de brownie <helado.de.brownie@gmail.com>
2019-07-07 20:21:52 -0400

    implement XdY parsing and methods

--- Dice.lua
+++ Dice.lua
@@ -1,11 +1,36 @@
 #!/usr/bin/env lua
 local Dice = {}
-Dice.__meta = Dice
+Dice.__index = Dice
 
 --# Interface
 
 function Dice.fromString(diceString)
-    error'UNIMPLEMENTED: Dice.fromString'
+    local quantity, size = diceString:match'(%d+)d(%d+)'
+
+    if quantity ~= nil and size ~= nil then
+        return Dice.new(tonumber(quantity, 10), tonumber(size, 10))
+    else
+        error'UNIMPLEMENTED: Dice.fromString'
+    end
+end
+
+function Dice.new(quantity, size)
+    return setmetatable({
+        quantity = quantity,
+        size = size,
+    }, Dice)
+end
+
+function Dice:minimum()
+    return self.quantity * 1
+end
+
+function Dice:mean()
+    return self.quantity * (1 + self.size) / 2
+end
+
+function Dice:maximum()
+    return self.quantity * self.size
 end
 
 --# Main
